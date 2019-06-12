14d8ad9487e638f9b1325aafa400d6fb494a8148
Nico Krause <nico@le-space.de>
2019-06-12 22:51:12 +0500

    version 0.1.6

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   summary: "accounts-password with additional storage for proof of existence on the Doichain blockchain",
   git: ' https://github.com/Doichain/meteor-accounts-password-doichain.git',
   documentation: 'README.md',
-  version: "0.1.5"
+  version: "0.1.6"
 });
 
 Package.onUse(api => {
